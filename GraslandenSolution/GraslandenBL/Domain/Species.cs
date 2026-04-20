1cbf843ce75a9c30f931725be8f8a0f6c4ca6dc2
dizzy <184835166+DizzyPho@users.noreply.github.com>
2026-04-20 10:12:35 +0200

    Add validation

--- GraslandenSolution/GraslandenBL/Domain/Species.cs
+++ GraslandenSolution/GraslandenBL/Domain/Species.cs
@@ -1,20 +1,21 @@
 ﻿using GraslandenBL.Enums;
+using GraslandenBL.FactoryResults;
 using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace GraslandenBL.Domain {
     public class Species {
-        public int Id { get; set; }
+        public int? Id { get; set; }
         public string Name { get; set; }
         public int Moisture { get; set; }
         public int Ph { get; set; }
         public int Nitrogen { get; set; }
         public int? Nectarvalue { get; set; }
         public int? Biodiversity { get; set; }
-        public Rating Rating { get; set; }
+        public Rating? Rating { get; set; }
 
-        private Species(int id, string name, int moisture, int ph, int nitrogen, int? nectarvalue, int? biodiversity, Rating rating) {
+        private Species(int? id, string name, int moisture, int ph, int nitrogen, int? nectarvalue, int? biodiversity, Rating? rating) {
             Id = id;
             Name = name;
             Moisture = moisture;
@@ -25,9 +26,26 @@ namespace GraslandenBL.Domain {
             Rating = rating;
         }
 
-        
+        public static FactoryResult<Species> Create(int? id, string name, int moisture, int ph, int nitrogen, int? nectarvalue, int? biodiversity, Rating? rating)
+        {
+            List<String> errors = new List<String>();
+            if (String.IsNullOrEmpty(name)) errors.Add("Naam mag niet leeg zijn.");
+            if (moisture < 0) errors.Add($"Foute waarde voor vochtigheid '{moisture}'. Moet een positief getal zijn.");
+            if (ph < 0) errors.Add($"Foute waarde voor zuurtegraad '{ph}'. Moet een positief getal zijn.");
+            if (nitrogen < 0) errors.Add($"Foute stikstofwaarde '{nitrogen}'. Moet een positief getal zijn.");
+            if (nectarvalue < 0) errors.Add($"Foute nectarwaarde '{nectarvalue}'. Moet een positief getal zijn.");
+            if (biodiversity < 0) errors.Add($"Foute biodiversiteitswaarde '{moisture}'. Moet een positief getal zijn.");
 
 
+            if (errors.Count > 0)
+            {
+                return new FactoryResult<Species>(errors);
+            }
+            else
+            {
+                return new FactoryResult<Species>(new Species(id, name, moisture, ph, nitrogen, nectarvalue, biodiversity, rating));
+            }
+        }
 
     }
 }
