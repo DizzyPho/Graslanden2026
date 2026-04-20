4d6b96bdcbfa383b6b4c6cbcd6fbe0e41d6bd40b
DriesOpalfvens <147134914+DriesOpalfvens@users.noreply.github.com>
2026-04-20 09:05:02 +0200

    Add MeasurementDTO

---
+++ GraslandenSolution/GraslandenBL/DTOs/MeasurementDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GraslandenBL.DTOs {
+    public class MeasurementDTO {
+        public int SpeciesId { get; set; }
+        public string Coverage { get; set; }
+        public MeasurementDTO(int speciesid, string coverage) {
+            SpeciesId = speciesid;
+            Coverage = coverage;
+        }
+
+
+    }
+}
