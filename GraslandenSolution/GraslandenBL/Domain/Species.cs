0c49acba5ffb73001a002b14718bd13be66fbcab
natanneyt <114665976+natanneyt@users.noreply.github.com>
2026-04-20 09:47:05 +0200

    Make id & rating nullable

--- GraslandenSolution/GraslandenBL/Domain/Species.cs
+++ GraslandenSolution/GraslandenBL/Domain/Species.cs
@@ -5,16 +5,16 @@ using System.Text;
 
 namespace GraslandenBL.Domain {
     public class Species {
-        public int Id { get; set; }
+        public int? Id { get; set; }
         public string Name { get; set; }
         public int Moisture { get; set; }
         public int Ph { get; set; }
         public int Nitrogen { get; set; }
         public int Nectarvalue { get; set; }
         public int Biodiversity { get; set; }
-        public Rating Rating { get; set; }
+        public Rating? Rating { get; set; }
 
-        public Species(int id, string name, int moisture,int ph, int biodiversity, Rating rating) {
+        public Species(int? id, string name, int moisture,int ph, int biodiversity, Rating? rating) {
             Id = id;
             Name = name;
             Moisture = moisture;
@@ -22,6 +22,8 @@ namespace GraslandenBL.Domain {
             biodiversity = biodiversity;
             Rating = rating;
         }
+
+
 
         
 
