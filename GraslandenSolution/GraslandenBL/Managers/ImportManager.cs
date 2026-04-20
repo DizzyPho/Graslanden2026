0abdadc74273a54ad98d22426ecae69e006d04ae
natanneyt <114665976+natanneyt@users.noreply.github.com>
2026-04-20 11:54:39 +0200

    Add import manager

---
+++ GraslandenSolution/GraslandenBL/Managers/ImportManager.cs
@@ -0,0 +1,23 @@
+﻿using GraslandenBL.Domain;
+using GraslandenBL.FactoryResults;
+using GraslandenBL.Interfaces;
+
+namespace GraslandenBL.Managers
+{
+    internal class ImportManager
+    {
+        private IRepository _repository;
+        private IFileReader _fileReader;
+
+        public ImportManager(IRepository repository, IFileReader fileReader)
+        {
+            _repository = repository;
+            _fileReader = fileReader;
+        }
+
+        public List<FactoryResult<Measurement>> ReadFile()
+        {
+            return _fileReader.ReadFile();
+        }
+    }
+}
