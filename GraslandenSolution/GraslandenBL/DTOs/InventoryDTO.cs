cdaddeae48349da125538518eb3b057a1eee0e75
dizzy <184835166+DizzyPho@users.noreply.github.com>
2026-04-20 08:56:41 +0200

    Add inventory DTO

---
+++ GraslandenSolution/GraslandenBL/DTOs/InventoryDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GraslandenBL.DTOs
+{
+    public class InventoryDTO
+    {
+        private InventoryDTO(DateTime date, string name)
+        {
+            Date = date;
+            Name = name;
+        }
+
+        public DateTime Date { get; set; }
+        public String Name { get; set; }
+    }
+}
