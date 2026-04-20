433b6874c5f958f5eeb7083c42c1611880286041
DriesOpalfvens <147134914+DriesOpalfvens@users.noreply.github.com>
2026-04-20 08:54:47 +0200

    Add Species class

---
+++ GraslandenSolution/GraslandenBL/Domain/Species.cs
@@ -0,0 +1,31 @@
+﻿using GraslandenBL.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GraslandenBL.Domain {
+    public class Species {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Moisture { get; set; }
+        public int Ph { get; set; }
+        public int Nitrogen { get; set; }
+        public int Nectarvalue { get; set; }
+        public int Biodiversity { get; set; }
+        public Rating Rating { get; set; }
+
+        public Species(int id, string name, int moisture,int ph, int biodiversity, Rating rating) {
+            Id = id;
+            Name = name;
+            Moisture = moisture;
+            Ph = ph;
+            biodiversity = biodiversity;
+            Rating = rating;
+        }
+
+        
+
+
+
+    }
+}
