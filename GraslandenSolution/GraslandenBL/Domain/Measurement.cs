21f92ffcbebd8e2efed548fa68c587846c3c3431
DriesOpalfvens <147134914+DriesOpalfvens@users.noreply.github.com>
2026-04-20 08:47:20 +0200

    Add measurement class

---
+++ GraslandenSolution/GraslandenBL/Domain/Measurement.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GraslandenBL.Domain {
+    internal class Measurement {
+        public Species Species { get; set; }
+        public string Coverage { get; set; }
+
+
+    }
+}
