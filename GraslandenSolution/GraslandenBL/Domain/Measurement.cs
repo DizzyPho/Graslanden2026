3ac13a7dda69834b52dd376ddc1e941cc8632ed1
DriesOpalfvens <147134914+DriesOpalfvens@users.noreply.github.com>
2026-04-20 08:49:00 +0200

    Add Plot property to measurement class

--- GraslandenSolution/GraslandenBL/Domain/Measurement.cs
+++ GraslandenSolution/GraslandenBL/Domain/Measurement.cs
@@ -6,7 +6,6 @@ namespace GraslandenBL.Domain {
     internal class Measurement {
         public Species Species { get; set; }
         public string Coverage { get; set; }
-
-
+        public Plot Plot { get; set; }
     }
 }
