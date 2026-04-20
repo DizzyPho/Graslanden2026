0f82e28a4598336cc1e4a1a53194bea99fbf5229
dizzy <184835166+DizzyPho@users.noreply.github.com>
2026-04-20 08:52:59 +0200

    Add inventory class

---
+++ GraslandenSolution/GraslandenBL/Domain/Inventory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GraslandenBL.Domain
+{
+    public class Inventory
+    {
+        public Inventory(DateTime date, string name)
+        {
+            Date = date;
+            Name = name;
+            Measurements = new List<Measurement>();
+        }
+
+        public DateTime Date { get; set; }
+        public String Name { get; set; }
+        public List<Measurement> Measurements { get; set; }
+
+
+    }
+}
