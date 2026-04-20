84076258717e426965718f7ce6f3d74b5f06d67b
dizzy <184835166+DizzyPho@users.noreply.github.com>
2026-04-20 11:06:04 +0200

    Validate plotType

--- GraslandenSolution/GraslandenBL/Domain/Plot.cs
+++ GraslandenSolution/GraslandenBL/Domain/Plot.cs
@@ -17,9 +17,11 @@ namespace GraslandenBL.Domain
             PlotType = plotType;
         }
 
-        public static FactoryResult<Plot> Create(string code, double areaSqMeters, string campus, ManagementType managementType, PlotType plotType)
+        public static FactoryResult<Plot> Create(string code, double areaSqMeters, string campus, ManagementType managementType, string plotTypeCode, string plotTypeDesc)
         {
             List<String> errors = new List<String>();
+            FactoryResult<PlotType> plotTypeFactory = PlotType.Create(plotTypeCode, plotTypeDesc);
+            errors.Concat(plotTypeFactory.Errors);
             if (String.IsNullOrEmpty(code)) errors.Add("Graslandcode mag niet leeg zijn.");
             if (areaSqMeters <= 0) errors.Add($"Ongeldige oppervlakte '${areaSqMeters}'. Moet een strikt positief getal zijn.");
             if (String.IsNullOrEmpty(campus)) errors.Add("Campusnaam mag niet leeg zijn.");
@@ -30,7 +32,7 @@ namespace GraslandenBL.Domain
             }
             else
             {
-                return new FactoryResult<Plot>(new Plot(code, areaSqMeters, campus, managementType, plotType));
+                return new FactoryResult<Plot>(new Plot(code, areaSqMeters, campus, managementType, plotTypeFactory.Result));
             }
         }
 
