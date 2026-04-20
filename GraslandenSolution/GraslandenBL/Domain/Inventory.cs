837ac8ae494486b940aca4eec3a0a0b73210ed7f
dizzy <184835166+DizzyPho@users.noreply.github.com>
2026-04-20 10:34:11 +0200

    Add validation

--- GraslandenSolution/GraslandenBL/Domain/Inventory.cs
+++ GraslandenSolution/GraslandenBL/Domain/Inventory.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GraslandenBL.FactoryResults;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -13,6 +14,21 @@ namespace GraslandenBL.Domain
             Measurements = new List<Measurement>();
         }
 
+        public static FactoryResult<Inventory> Create(DateTime date, String name) 
+        {
+            List<String> errors = new List<String>();
+            if (date > DateTime.Now) errors.Add("Datum mag niet in de toekomst zijn.");
+            if (String.IsNullOrEmpty(name)) errors.Add("Naam mag niet leeg zijn.");
+
+            if(errors.Count > 0)
+            {
+                return new FactoryResult<Inventory>(errors);
+            }
+            else
+            {
+                return new FactoryResult<Inventory>(new Inventory(date, name));
+            }
+        }
         public DateTime Date { get; set; }
         public String Name { get; set; }
         public List<Measurement> Measurements { get; set; }
--- GraslandenSolution/GraslandenBL/Domain/Plot.cs
+++ GraslandenSolution/GraslandenBL/Domain/Plot.cs
@@ -1,4 +1,5 @@
 ﻿using GraslandenBL.Enums;
+using GraslandenBL.FactoryResults;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -7,13 +8,30 @@ namespace GraslandenBL.Domain
 {
     public class Plot
     {
-        public Plot(string code, double areaSqMeters, string campus, ManagementType managementType, PlotType plotType)
+        private Plot(string code, double areaSqMeters, string campus, ManagementType managementType, PlotType plotType)
         {
             Code = code;
             AreaSqMeters = areaSqMeters;
             Campus = campus;
             ManagementType = managementType;
             PlotType = plotType;
+        }
+
+        public static FactoryResult<Plot> Create(string code, double areaSqMeters, string campus, ManagementType managementType, PlotType plotType)
+        {
+            List<String> errors = new List<String>();
+            if (String.IsNullOrEmpty(code)) errors.Add("Graslandcode mag niet leeg zijn.");
+            if (areaSqMeters <= 0) errors.Add($"Ongeldige oppervlakte '${areaSqMeters}'. Moet een strikt positief getal zijn.");
+            if (String.IsNullOrEmpty(campus)) errors.Add("Campusnaam mag niet leeg zijn.");
+
+            if (errors.Count > 0)
+            {
+                return new FactoryResult<Plot>(errors);
+            }
+            else
+            {
+                return new FactoryResult<Plot>(new Plot(code, areaSqMeters, campus, managementType, plotType));
+            }
         }
 
         public string Code { get; set;  }
