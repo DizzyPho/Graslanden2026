f374b6a571ecb59d52d581b4de29538968e6ac48
dizzy <184835166+DizzyPho@users.noreply.github.com>
2026-04-20 08:43:40 +0200

    Add PlotType Class

---
+++ GraslandenSolution/GraslandenBL/Domain/PlotType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GraslandenBL.Domain
+{
+    public class PlotType
+    {
+        private PlotType(string code, string description)
+        {
+            Code = code;
+            Description = description;
+        }
+
+        public string Code { get; set; }
+        public string Description { get; set; }
+    }
+}
