a340aec63403b97c5907246a7c318f81fcfc045e
dizzy <184835166+DizzyPho@users.noreply.github.com>
2026-04-20 09:46:17 +0200

    Add FactoryResult<T>

---
+++ GraslandenSolution/GraslandenBL/FactoryResults/FactoryResult.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GraslandenBL.FactoryResults
+{
+    public class FactoryResult<T>
+    {
+        public T Result { get; private set; }
+        public List<String> Errors { get; init; }
+        public bool IsSuccess { get => Errors.Count == 0; }
+    }
+}
