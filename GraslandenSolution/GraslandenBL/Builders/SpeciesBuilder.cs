0eb38374c40dc5f2a47e9f8057d0576d179e8ca7
natanneyt <114665976+natanneyt@users.noreply.github.com>
2026-04-21 10:39:25 +0200

    Add SpeciesBuilder error messages to list in repository

---
+++ GraslandenSolution/GraslandenBL/Builders/SpeciesBuilder.cs
@@ -0,0 +1,130 @@
+﻿using GraslandenBL.Domain;
+using GraslandenBL.Enums;
+using GraslandenBL.Results;
+
+namespace GraslandenBL.Builders
+{
+    public class SpeciesBuilder
+    {
+        private Species _species;
+        private List<string> _errors;
+        private string _locationWithinFile;
+
+        public SpeciesBuilder(string name, string locationWithinFile)
+        {
+            _locationWithinFile = locationWithinFile;
+            _errors = new List<string>();
+
+            _species = new Species();
+            if (string.IsNullOrEmpty(name)) _errors.Add($"{_locationWithinFile} | Naam mag niet leeg zijn.");
+            else _species.Name = name;
+        }
+
+        public SpeciesBuilder AddMoisture(string moistureString)
+        {
+            int? moisture = null;
+
+            try
+            {
+                if (!string.IsNullOrEmpty(moistureString)) _species.Moisture = int.Parse(moistureString);
+                if (moisture < 0) _errors.Add($"{_locationWithinFile} | {_species.Name} | Foute waarde voor vochtigheid '{moisture}'. Moet een positief getal zijn.");
+            }
+            catch
+            {
+                _errors.Add($"{_locationWithinFile} | {_species.Name} | Vochtgehalte: '{moistureString}' moet een getal zijn.");
+            }
+            return this;
+        }
+
+        public SpeciesBuilder AddPh(string phString)
+        {
+            int? ph = null;
+
+            try
+            {
+                if (!string.IsNullOrEmpty(phString)) _species.Ph = int.Parse(phString);
+                if (ph < 0) _errors.Add($"{_locationWithinFile} | {_species.Name} | Foute waarde voor zuurtegraad '{ph}'. Moet een positief getal zijn.");
+            }
+            catch
+            {
+                _errors.Add($"{_locationWithinFile} | {_species.Name} | Zuurtegraad: '{phString}' moet een getal zijn.");
+            }
+            return this;
+        }
+
+        public SpeciesBuilder AddNitrogen(string nitrogenString)
+        {
+            int? nitrogen = null;
+
+            try
+            {
+                if (!string.IsNullOrEmpty(nitrogenString)) _species.Nitrogen = int.Parse(nitrogenString);
+                if (nitrogen < 0) _errors.Add($"{_locationWithinFile} | {_species.Name} | Foute stikstofwaarde '{nitrogen}'. Moet een positief getal zijn.");
+            }
+            catch
+            {
+                _errors.Add($"{_locationWithinFile} | {_species.Name} | Stikstofgehalte: '{nitrogenString}' moet een getal zijn.");
+            }
+            return this;
+        }
+
+        public SpeciesBuilder AddNectarValue(string nectarValueString)
+        {
+            int? nectarValue = null;
+
+            try
+            {
+                if (!string.IsNullOrEmpty(nectarValueString)) _species.Nectarvalue = int.Parse(nectarValueString);
+                if (nectarValue < 0) _errors.Add($"{_locationWithinFile} | {_species.Name} | Foute nectarwaarde '{nectarValue}'. Moet een positief getal zijn.");
+            }
+            catch
+            {
+                _errors.Add($"{_locationWithinFile} | {_species.Name} | Nectarwaarde: '{nectarValueString}' moet een getal zijn.");
+            }
+            return this;
+        }
+
+        public SpeciesBuilder AddBiodiversity(string biodiversityString)
+        {
+            int? biodiversity = null;
+
+            try
+            {
+                if (!string.IsNullOrEmpty(biodiversityString)) _species.Biodiversity = int.Parse(biodiversityString);
+                if (biodiversity < 0) _errors.Add($"{_locationWithinFile} | {_species.Name} | Foute biodiversiteitswaarde '{biodiversity}'. Moet een positief getal zijn.");
+            }
+            catch
+            {
+                _errors.Add($"{_locationWithinFile} | {_species.Name} | Biodiversiteit: '{biodiversityString}' moet een getal zijn.");
+            }
+            return this;
+        }
+
+        public SpeciesBuilder AddRating(string ratingString)
+        {
+            Rating? rating = null;
+            try
+            {
+                _species.Rating = ratingString.Trim() switch
+                {
+                    "+++" => Rating.Sleutel,
+                    "++" => Rating.Begeleidend,
+                    "+" => Rating.Algemeen,
+                    "0" => Rating.Ruderaal,
+                    "-" => Rating.Invasief
+                };
+            }
+            catch
+            {
+                _errors.Add($"{_locationWithinFile} | {_species.Name} | Rating: '{ratingString}' is ongeldig.");
+            }
+            return this;
+        }
+
+        public SpeciesResult Build()
+        {
+            if (!string.IsNullOrEmpty(_species.Name)) return new SpeciesResult(_species, _errors);
+            else return new SpeciesResult(null, _errors);
+        }
+    }
+}
--- GraslandenSolution/GraslandenBL/Domain/Species.cs
+++ GraslandenSolution/GraslandenBL/Domain/Species.cs
@@ -7,14 +7,14 @@ namespace GraslandenBL.Domain {
     public class Species {
         public int? Id { get; set; }
         public string Name { get; set; }
-        public int Moisture { get; set; }
-        public int Ph { get; set; }
-        public int Nitrogen { get; set; }
+        public int? Moisture { get; set; }
+        public int? Ph { get; set; }
+        public int? Nitrogen { get; set; }
         public int? Nectarvalue { get; set; }
         public int? Biodiversity { get; set; }
         public Rating? Rating { get; set; }
 
-        public Species(int? id, string name, int moisture, int ph, int nitrogen, int? nectarvalue, int? biodiversity, Rating? rating) {
+        public Species(int? id, string name, int? moisture, int? ph, int? nitrogen, int? nectarvalue, int? biodiversity, Rating? rating) {
             Id = id;
             Name = name;
             Moisture = moisture;
@@ -24,6 +24,8 @@ namespace GraslandenBL.Domain {
             Biodiversity = biodiversity;
             Rating = rating;
         }
+
+        public Species() { }
 
     }
 }
