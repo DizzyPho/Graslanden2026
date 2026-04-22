e1dad6168e3028b098d0ac4d8c46aece8c4fef99
natanneyt <114665976+natanneyt@users.noreply.github.com>
2026-04-22 12:34:25 +0200

    Add MessageType

--- GraslandenSolution/GraslandenBL/Enums/Enums.cs
+++ GraslandenSolution/GraslandenBL/Enums/Enums.cs
@@ -6,4 +6,6 @@ namespace GraslandenBL.Enums
 {
     public enum ManagementType { Netheidsboord, Schapenweide, Intensief, Extensief}
     public enum Rating { Sleutel, Begeleidend, Algemeen, Ruderaal, Invasief }
+
+    public enum MessageType { Remark, Error }
 }
