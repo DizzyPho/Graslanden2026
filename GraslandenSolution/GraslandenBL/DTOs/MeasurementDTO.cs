2632642df51464ca603fb64dcf9d0ccf4d534b0f
dizzy <184835166+DizzyPho@users.noreply.github.com>
2026-04-22 19:45:14 +0200

    Change property to type Species instead of int

--- GraslandenSolution/GraslandenBL/DTOs/MeasurementDTO.cs
+++ GraslandenSolution/GraslandenBL/DTOs/MeasurementDTO.cs
@@ -1,13 +1,14 @@
-﻿using System;
+﻿using GraslandenBL.Domain;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace GraslandenBL.DTOs {
     public class MeasurementDTO {
-        public int SpeciesId { get; set; }
+        public Species Species { get; set; }
         public string Coverage { get; set; }
-        public MeasurementDTO(int speciesid, string coverage) {
-            SpeciesId = speciesid;
+        public MeasurementDTO(Species species, string coverage) {
+            Species = species;
             Coverage = coverage;
         }
 
