3e421fdb93e6d63dd0796366db9a1cb9421a124b
natanneyt <114665976+natanneyt@users.noreply.github.com>
2026-04-22 08:42:38 +0200

    Add CampusDTO with PlotValue record struct

---
+++ GraslandenSolution/GraslandenBL/DTOs/CampusDTO.cs
@@ -0,0 +1,30 @@
+﻿using GraslandenBL.Domain;
+
+namespace GraslandenBL.DTOs
+{
+    public class CampusDTO
+    {
+        public List<Plot> Plots { get; set; }
+
+        public Dictionary<string, PlotValue> PlotTypes { get; set; }
+
+        public CampusDTO()
+        {
+            Plots = new List<Plot>();
+            PlotTypes = new Dictionary<string, PlotValue>();
+        }
+
+        public CampusDTO(List<Plot> plots, Dictionary<string, PlotValue> plotTypes)
+        {
+            Plots = plots;
+            PlotTypes = plotTypes;
+        }
+    }
+
+    public record struct PlotValue
+    {
+        public int SpeciesCount { get; set; }
+
+        public double TotalAreaSqMeters { get; set; }
+    }
+}
