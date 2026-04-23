b05c5061ccb9fb3a7308b52698c3f274aa991f04
natanneyt <114665976+natanneyt@users.noreply.github.com>
2026-04-23 14:56:39 +0200

    Make MessageDTO

---
+++ GraslandenSolution/GraslandenBL/DTOs/MessageDTO.cs
@@ -0,0 +1,21 @@
+﻿namespace GraslandenBL.DTOs
+{
+    public class MessageDTO
+    {
+        public int? ObjectId { get; set; }
+
+        public int InventoryId { get; set; }
+
+        public string Description { get; set; }
+
+        public string MessageType { get; set; }
+
+        public MessageDTO(int? objectId,  int inventoryId, string description, string messageType)
+        {
+            ObjectId = objectId;
+            InventoryId = inventoryId;
+            Description = description;
+            MessageType = messageType;
+        }
+    }
+}
