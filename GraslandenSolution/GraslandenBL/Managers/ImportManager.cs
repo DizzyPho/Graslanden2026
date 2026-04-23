6c72e7be8b40b0c588d52a6b33c64dda90092a57
natanneyt <114665976+natanneyt@users.noreply.github.com>
2026-04-23 11:03:51 +0200

    Separate reading of file from importing

--- GraslandenSolution/GraslandenBL/Managers/ImportManager.cs
+++ GraslandenSolution/GraslandenBL/Managers/ImportManager.cs
@@ -9,28 +9,50 @@ namespace GraslandenBL.Managers
     {
         private IRepository _repository;
         private IFileReader _fileReader;
+        private List<Measurement> _measurements;
+        private Dictionary<string, MessageType> _messages;
 
         public ImportManager(IRepository repository, IFileReader fileReader)
         {
             _repository = repository;
             _fileReader = fileReader;
+            _measurements = new List<Measurement>();
+            _messages = new Dictionary<string, MessageType>();
+        }
+
+        public bool ReadFile(string inventoryPath)
+        {
+            try
+            {
+                _measurements = _fileReader.ReadFile(inventoryPath, out Dictionary<string, MessageType> incomingMessages);
+                _messages = incomingMessages;
+                return true;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+                return false;
+            }
         }
 
         // returns ID of inventory in DB
         public InventoryDTO ImportData(string inventoryPath, InventoryDTO inventoryDTO)
         {
-            Inventory inventory = new Inventory(inventoryDTO.Date, inventoryDTO.Name);
-            List<Measurement> measurements = _fileReader.ReadFile(inventoryPath, out Dictionary<string, MessageType> messages);
-            inventory.Measurements = measurements;
-            int inventoryId = _repository.ImportInventory(inventory);
-            inventoryDTO.Id = inventoryId;
-
-            if(messages.Count > 0)
+            if (_measurements.Count > 0)
             {
-                _repository.InsertMessages(inventoryId, messages);
-            }
+                Inventory inventory = new Inventory(inventoryDTO.Date, inventoryDTO.Name);
+                inventory.Measurements = _measurements;
+                int inventoryId = _repository.ImportInventory(inventory);
+                inventoryDTO.Id = inventoryId;
 
-            return inventoryDTO;
+                if (_messages.Count > 0)
+                {
+                    _repository.InsertMessages(inventoryId, _messages);
+                }
+
+                return inventoryDTO;
+            }
+            else return null;
         }
     }
 }
