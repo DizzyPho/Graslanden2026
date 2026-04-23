4870c4a085ae6a42d26a677dee4f886d6d08863a
dizzy <184835166+DizzyPho@users.noreply.github.com>
2026-04-23 14:19:02 +0200

    Add plot in UI

---
+++ GraslandenSolution/GraslandenGUI/Windows/AddPlotWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace GraslandenGUI.Windows
+{
+    /// <summary>
+    /// Interaction logic for AddPlotWindow.xaml
+    /// </summary>
+    public partial class AddPlotWindow : Window
+    {
+        public AddPlotWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonBack_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void ButtonAdd_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+    }
+}
