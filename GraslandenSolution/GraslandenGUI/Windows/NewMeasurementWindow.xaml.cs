bdf64e141a85ecd98f47680931a6b299bc7bbd3e
dizzy <184835166+DizzyPho@users.noreply.github.com>
2026-04-23 10:39:30 +0200

    Add window for inserting measurement

---
+++ GraslandenSolution/GraslandenGUI/Windows/NewMeasurementWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace GraslandenGUI.Windows
+{
+    /// <summary>
+    /// Interaction logic for NewMeasurementWindow.xaml
+    /// </summary>
+    public partial class NewMeasurementWindow : Window
+    {
+        public NewMeasurementWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonAdd_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ButtonBack_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
