34eb94a3ad385bc0ed5d860cdc783d8a765c361c
DriesOpalfvens <147134914+DriesOpalfvens@users.noreply.github.com>
2026-04-23 12:13:44 +0200

    Add method InsertInventoriedPlot

--- GraslandenSolution/GraslandenBL/Interfaces/IRepository.cs
+++ GraslandenSolution/GraslandenBL/Interfaces/IRepository.cs
@@ -27,6 +27,6 @@ namespace GraslandenBL.Interfaces
 
         public void DeleteMeasurement(int measurementDTO_id);
 
-        public bool CheckIfPlotExsists(string code);
+        public bool InsertInventoriedPlot(int inventoryID, string code, ManagementType managementType, string plot_Type)
     }
 }
